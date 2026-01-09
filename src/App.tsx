cc1c2038269a9b0ce97d0837a661d863c19cc007
sovilleda07 <sovilleda07@gmail.com>
2026-01-09 17:46:02 -0600

    Create TodoList component, add delete todo and sorting completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import TodoItem from "./components/ToDoItem";
 import { dummyData } from "./data/todos";
 import AddToForm from "./components/AddTodoForm";
+import TodoList from "./components/TodoList";
 
 function App() {
   const [todos, setTodos] = useState(dummyData);
@@ -23,20 +23,20 @@ function App() {
     ]);
   }
 
+  function deleteTodo(id: number) {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  }
+
   return (
-    <main className="py-10 h-screen space-y-5">
+    <main className="py-10 h-screen space-y-5 overflow-y-auto">
       <h1 className="font-bold text-3xl text-center">Your Todos</h1>
       <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
         <AddToForm onSubmit={addTodo} />
-        <div className="space-y-2">
-          {todos.map((todo) => (
-            <TodoItem
-              key={todo.id}
-              todo={todo}
-              onCompletedChange={setTodoCompleted}
-            />
-          ))}
-        </div>
+        <TodoList
+          todos={todos}
+          onCompletedChange={setTodoCompleted}
+          onDelete={deleteTodo}
+        />
       </div>
     </main>
   );
