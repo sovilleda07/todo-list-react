461e25c8cda126e4f0e1663166a93a1ebbb3e25e
sovilleda07 <sovilleda07@gmail.com>
2026-01-09 18:04:25 -0600

    Create TodoSummary component and delete all todos completed

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { dummyData } from "./data/todos";
 import AddToForm from "./components/AddTodoForm";
 import TodoList from "./components/TodoList";
+import TodoSummary from "./components/TodoSummary";
 
 function App() {
   const [todos, setTodos] = useState(dummyData);
@@ -15,7 +16,7 @@ function App() {
   function addTodo(title: string) {
     setTodos((prevTodos) => [
       {
-        id: prevTodos.length + 1,
+        id: Date.now(),
         title,
         completed: false,
       },
@@ -25,6 +26,10 @@ function App() {
 
   function deleteTodo(id: number) {
     setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  }
+
+  function deleteAllCompletedTodos() {
+    setTodos((prevTodos) => prevTodos.filter((todo) => !todo.completed));
   }
 
   return (
@@ -38,6 +43,7 @@ function App() {
           onDelete={deleteTodo}
         />
       </div>
+      <TodoSummary todos={todos} deleteAllCompleted={deleteAllCompletedTodos} />
     </main>
   );
 }
